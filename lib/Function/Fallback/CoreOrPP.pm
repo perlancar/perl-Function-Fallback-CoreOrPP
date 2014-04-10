45514bea57f5380879d8f81e5da26969b8864ab9
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-04-10 12:40:40 +0700

    Rename dist from SHARYANTO-MaybeXS to Function-Fallback-CoreOrPP

--- lib/Function/Fallback/CoreOrPP.pm
+++ lib/Function/Fallback/CoreOrPP.pm
@@ -1,4 +1,4 @@
-package SHARYANTO::MaybeXS;
+package Function::Fallback::CoreOrPP;
 
 use 5.010001;
 use strict;
@@ -6,7 +6,7 @@ use warnings;
 
 # VERSION
 
-our $USE_XS = 1;
+our $USE_NONCORE_XS_FIRST = 1;
 
 require Exporter;
 our @ISA       = qw(Exporter);
@@ -17,7 +17,7 @@ our @EXPORT_OK = qw(
 
 sub clone {
     my $data = shift;
-    goto FALLBACK unless $USE_XS;
+    goto FALLBACK unless $USE_NONCORE_XS_FIRST;
     goto FALLBACK unless eval { require Data::Clone; 1 };
 
   STANDARD:
@@ -29,7 +29,7 @@ sub clone {
 }
 
 sub uniq {
-    goto FALLBACK unless $USE_XS;
+    goto FALLBACK unless $USE_NONCORE_XS_FIRST;
     goto FALLBACK unless eval { require List::MoreUtils; 1 };
 
   STANDARD:
@@ -45,37 +45,41 @@ sub uniq {
 }
 
 1;
-#ABSTRACT: Do task using non-core XS module, but provide pure-Perl/core fallback
+#ABSTRACT: Functions that use non-core XS module but provide pure-Perl/core fallback
 
 =for Pod::Coverage ^()$
 
 =head1 SYNOPSIS
 
- use SHARYANTO::MaybeXS qw(clone uniq);
+ use Function::Fallback::CoreOrPP qw(clone uniq);
 
  my $clone = clone({blah=>1});
+ my @uniq  = uniq(1, 3, 2, 1, 4);  # -> (1, 3, 2, 4)
 
 
 =head1 DESCRIPTION
 
+This module provides functions that use non-core XS modules (for best speed,
+reliability, feature, etc) but falls back to those that use core XS or pure-Perl
+modules when the non-core XS module is not available.
+
 This module helps when you want to bootstrap your Perl application with a
 portable, dependency-free Perl script. In a vanilla Perl installation (having
-only core modules), you can use L<App::FatPacker> to include pure-Perl
-dependencies to your script. This module provides fallback for some tasks that
-usually need to be done using a non-core XS module, by providing alternatives
-using pure-Perl or core XS module.
+only core modules), you can use L<App::FatPacker> to include non-core pure-Perl
+dependencies to your script.
 
 
 =head1 FUNCTIONS
 
 =head2 clone($data) => $cloned
 
-Try to use L<Data::Clone>'s C<clone>, but fallback to L<Clone::PP>'s C<clone>.
+Try to use L<Data::Clone>'s C<clone> (because it's the fastest) but, when not
+available, fall back to L<Clone::PP>'s C<clone>.
 
 =head2 uniq(@ary) => @uniq_ary
 
-Try to use L<List::MoreUtils>'s C<uniq>, but fallback to using pure-Perl
-implementation.
+Try to use L<List::MoreUtils>'s C<uniq>, but fall back to using slower,
+pure-Perl implementation.
 
 
 =head1 SEE ALSO
--- t/01-basics.t
+++ t/01-basics.t
@@ -4,13 +4,11 @@ use 5.010;
 use strict;
 use warnings;
 
+use Function::Fallback::CoreOrPP qw(clone uniq);
 use Test::More 0.98;
 
-use SHARYANTO::MaybeXS qw(clone uniq);
-
-# XXX test uniq
-
 subtest "uniq" => sub {
+    local $Function::Fallback::CoreOrPP::USE_NONCORE_XS_FIRST = 0;
     is_deeply([uniq(1, 3, 2, 1, 3, 4)], [1, 3, 2, 4]);
 };
 
